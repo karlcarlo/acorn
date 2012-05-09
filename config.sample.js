b2c53da11991826a3f27089c1d29df3a4ef2cfb3
karlcarlo <karlcarlo@163.com>
2012-05-09 20:43:08 +0800

    fixed default host

--- config.sample.js
+++ config.sample.js
@@ -2,7 +2,7 @@ exports.config = {
   application: {
     version: 'v0.2.2',
     name: "Acorn",
-    host: 'http://ux.sohu.com/',
+    host: 'http://127.0.0.1/',
     port: 3000,
     root_account: 'root@localhost'
   },
@@ -10,7 +10,7 @@ exports.config = {
     secret: "acorn_blog"
   },
   database: {
-	url: "mongodb://127.0.0.1/acorn_blog"
+    url: "mongodb://127.0.0.1/acorn_blog"
   },
   cookie: {
     name: 'acorn'
