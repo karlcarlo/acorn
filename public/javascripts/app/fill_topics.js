6358879878473110f6c5f57b672dd734f00f6803
karlcarlo <karlcarlo@163.com>
2013-01-09 19:08:46 +0800

    up

--- public/javascripts/app/fill_topics.js
+++ public/javascripts/app/fill_topics.js
@@ -1,28 +1,33 @@
 define(['jquery'], function($){
   return function(options){
 
-      var settings = $.extend({
-        container: '.sidebar > .latest-topics'
-      }, options || {});
+    var settings = $.extend({
+      container: '.sidebar > .latest-topics'
+    }, options || {});
 
-      // 填充标签
-      var $container = $(settings.container);
-      if($container.length){
-        $.get('/topics/latest.json', function(json){
-          if(!json || !json.success){
-            return;
-          }
+    // 填充标签
+    var $container = $(settings.container);
+    if($container.length){
+      $.get('/topics/latest.json', function(json){
+        if(!json || !json.success){
+          return;
+        }
 
-          var str = []
-            , topics = json.topics;
+        var str = []
+          , topics = json.topics;
 
-          for(var i = 0; i < topics.length; i++){
-            str.push('<a href="/topics/' + topics[i]._id + '" title="' + topics[i].title + '">' + topics[i].title.substr(0, 30) + '</a>');
-          }
+        for(var i = 0; i < topics.length; i++){
+          str.push('<a href="/topics/' + topics[i]._id + '" title="' + topics[i].title + '">' + substr(topics[i].title, 30) + '</a>');
+        }
 
-          $container.html(str.join(''));
+        $container.html(str.join(''));
 
-        });
-      }
+      });
+    }
+
+    function substr(str, size){
+      if(str.length >= size) return str;
+      return str.substr(0, size - 2) + '..'
+    }
   };
 });
